093347fd21b9d284548ee357101bfd37e08173f0
Sheape <86521166+Sheape@users.noreply.github.com>
2023-07-18 11:31:23 +0800

    Done with quiz #1!

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount_bought: u8) -> u8{
+    let mut cost: u8 = 2;
+    if amount_bought > 40 {
+        cost = 1;
+    }
+    return amount_bought * cost;
+}
 
 // Don't modify this function!
 #[test]
